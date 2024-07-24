8f6e67dbae2ac02be0ae04e4a5cfae1b85b7ad89
Alejandro Vélez <141779507+MelonConYogurt@users.noreply.github.com>
2024-07-23 20:48:09 -0500

    update api
    se crearon nuevos modelos y nuevos metodos post

--- ven_boletos/SRC/api/api_cine.py
+++ ven_boletos/SRC/api/api_cine.py
@@ -6,18 +6,18 @@ from SRC.main.data_base_cine import *
 app = FastAPI()
 
 # ---Conexion de la base de datos------
-
 base_datos = conexion_base_datos()
 
-@app.get("/salas/")
-async def obtener_salas():
-    lista_salas = base_datos.buscar_salas()
-    if lista_salas:
-        return lista_salas
-    else:
-        raise HTTPException(status_code=404, detail="No se encontraron salas")
 
-@app.post("/asientos/")
+# @app.get("//")
+# async def obtener_salas():
+#     lista_salas = base_datos.buscar_salas()
+#     if lista_salas:
+#         return lista_salas
+#     else:
+#         raise HTTPException(status_code=404, detail="No se encontraron salas")
+
+@app.post("/crear/asientos/")
 async def agregar_asientos(asiento: model_asiento):
     nuevo_asiento = base_datos.anadir_asientos(
         nombre_sala = asiento.sala,
@@ -29,6 +29,60 @@ async def agregar_asientos(asiento: model_asiento):
         return asiento
     else:
         raise HTTPException(status_code=400, detail="No se pudo añadir el asiento")
+
+@app.post("/crear/salas/")
+async def agregar_salas(sala: model_sala):
+    try:
+        nueva_sala = base_datos.anadir_sala(
+            nombre_sala = sala.nombre_sala,
+            capacidad_sala = sala.capacidad_sala,
+            estado_sala = sala.estado_sala 
+        )
+        if nueva_sala:
+            return sala
+        else:
+            raise HTTPException(status_code=400, detail="No se pudo añadir el asiento")
+    except Exception as e:
+        pass
+                
+@app.post("/crear/peliculas/")
+async def agregar_peliculas(pelicula: model_pelicula):
+    nueva_pelicula = base_datos.anadir_pelicula(
+        titulo_pelicula = pelicula.titulo_pelicula,
+        genero_pelicula = pelicula.genero_pelicula,
+        duracion_pelicula = pelicula.duracion_pelicula,
+        rating_pelicula = pelicula.rating_pelicula
+        )
+    if nueva_pelicula:
+        return  pelicula
+    else:
+        raise HTTPException(status_code=400, detail="No se pudo añadir el asiento")
+        
+    
+@app.post("/crear/clientes/")
+async def agregar_cliente(cliente: model_clientes):
+    nuevo_cliente = base_datos.anadir_cliente(
+        nombre_cliente = cliente.nombre_cliente,
+        apellido_cliente = cliente.apellido_cliente,
+        correo_cliente = cliente.correo_cliente,
+        documento = cliente.documento_cliente
+        )
+    if nuevo_cliente:
+        return cliente
+    else:
+         raise HTTPException(status_code=400, detail="No se pudo añadir el asiento")
+        
+@app.post("/crear/horarios/")
+async def agregar_horarios(horario: model_horarios):
+    nuevo_horario = base_datos.anadir_horarios(
+        nombre_pelicula = horario.nombre_pelicula,
+        nombre_sala = horario.nombre_sala,
+        fecha_date = horario.fecha_funcion
+    )
+    if nuevo_horario:
+        return horario
+    else:
+         raise HTTPException(status_code=400, detail="No se pudo añadir el asiento")
 
 if __name__ == "__main__":
     pass
--- ven_boletos/SRC/api/modelos.py
+++ ven_boletos/SRC/api/modelos.py
@@ -8,6 +8,30 @@ class model_asiento(BaseModel):
     seccion_asiento: str
     fecha_asiento: datetime
 
+class model_sala(BaseModel):
+    nombre_sala: str
+    capacidad_sala: int
+    estado_sala: int
+    
+class model_pelicula(BaseModel):
+    titulo_pelicula: str
+    genero_pelicula: str
+    duracion_pelicula: int 
+    rating_pelicula: int
 
+class model_clientes(BaseModel):
+    nombre_cliente: str
+    apellido_cliente: str
+    correo_cliente: str
+    documento_cliente: str
+
+class model_horarios(BaseModel):
+    nombre_pelicula: str
+    nombre_sala: str
+    fecha_funcion: any
+    
+    
+    
+    
 if __name__ == "__main__":
     pass
